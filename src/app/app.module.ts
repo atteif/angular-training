b9851e211bcb7bf52956205f0185c09d2966c268
Atef Guesmi <g-atef@live.fr>
2020-07-06 17:52:54 +0100

    Complete the basics 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { from } from 'rxjs';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,17 +3,28 @@ import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`.online{
+            color:white;
+    }`]
   })
 export class ServerComponent{
   serverId: number= 10;
   serverStatus: string= 'offline';
 
-  
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+
   getServerStatus() {
     return this.serverStatus;
   }
-  
+
 
 
 }
